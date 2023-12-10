2b8a72b63806a2fd9c20085326f3e973c964146f
Alireza Doostimehr <alirezadoostimehr@gmail.com>
2023-12-11 01:16:53 +0330

    feat: added create get contest handler

--- api/contests_handlers.go
+++ api/contests_handlers.go
@@ -2,11 +2,45 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"ocontest/pkg"
+	"ocontest/pkg/structs"
+	"strconv"
 )
 
-func (h *handlers) CreateContest(c *gin.Context) {}
+func (h *handlers) CreateContest(c *gin.Context) {
+	logger := pkg.Log.WithField("handler", "createContest")
 
-func (h *handlers) GetContest(c *gin.Context) {}
+	var reqData structs.RequestCreateContest
+	if err := c.ShouldBindJSON(&reqData); err != nil {
+		logger.Warn("Failed to read request body", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": pkg.ErrBadRequest.Error(),
+		})
+		return
+	}
+
+	resp, status := h.contestHandler.CreateContest(c, reqData)
+	c.JSON(status, resp)
+}
+
+func (h *handlers) GetContest(c *gin.Context) {
+	_ = pkg.Log.WithField("handler", "getContest")
+
+	contestID, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid id, id should be an integer",
+		})
+		return
+	}
+	resp, status := h.contestHandler.GetContest(c, contestID)
+	if status == http.StatusOK {
+		c.JSON(status, resp)
+	} else {
+		c.Status(status)
+	}
+}
 
 func (h *handlers) ListContests(c *gin.Context) {}
 
